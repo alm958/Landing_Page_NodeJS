26e2ea41e09d1f4d9aeaba59c59f4ff7e1e5033d
alm958 <alm958@gmail.com>
2016-11-12 15:21:11 -0500

    fixed loadPage function

--- apps.js
+++ apps.js
@@ -1,54 +1,30 @@
 var http = require('http');
 var fs = require('fs');
 
-function loadPage(page){
-    console.log(page);
-    console.log(typeof(page));
+function loadPage(page, response, code){
     fs.readFile(page, 'utf8', function (errors, contents){
-        response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+        response.writeHead(code, {'Content-Type': 'text/html'});  // send data about response
         response.write(contents);  //  send response body
         response.end(); // finished!
     });
 }
-// creating a server using http module:
+
 var server = http.createServer(function (request, response){
-    // see what URL the clients are requesting:
-    console.log('client request URL: ', request.url);
-    // this is how we do routing:
     switch (request.url)
     {
         case '/':
-            loadPage('index.html');
-            // fs.readFile('index.html', 'utf8', function (errors, contents){
-            //     response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
-            //     response.write(contents);  //  send response body
-            //     response.end(); // finished!
-            // });
+            loadPage('index.html', response, 200);
             break;
         case '/ninjas':
-            fs.readFile('ninjas.html', 'utf8', function (errors, contents){
-                response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
-                response.write(contents);  //  send response body
-                response.end(); // finished!
-            });
+            loadPage('ninjas.html', response, 200);
             break;
         case '/dojos/new':
-            fs.readFile('dojos.html', 'utf8', function (errors, contents){
-                response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
-                response.write(contents);  //  send response body
-                response.end(); // finished!
-            });
+            loadPage('dojos.html', response, 200);
             break;
         default:
-            fs.readFile('error.html', 'utf8', function (errors, contents){
-            response.writeHead(404, {'Content-Type': 'text/html'});  // send data about response
-            response.write(contents);  //  send response body
-            response.end(); // finished!
-            });
+            loadPage('error.html', response, 404);
             break;
     };
 });
-// tell your server which port to run on
+
 server.listen(6789);
-// print to terminal window
-console.log("Running in localhost at port 6789");
